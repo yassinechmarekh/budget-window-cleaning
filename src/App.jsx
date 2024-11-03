965032a750884f13079e5d12ac0fc1d1e17f54f7
yassine <yassinechmarekh88@gmail.com>
2024-11-03 23:16:54 +0100

    add btn back to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import Banner from "./components/banner/banner"
-import Faq from "./components/faq/Faq"
-import Header from "./components/header/Header"
-import Hero from "./components/hero/Hero"
-import Services from "./components/services/Services"
-import Testimoniels from "./components/testimoniels/Testimoniels"
-import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs"
-import Footer from "./components/footer/Footer"
+import Banner from "./components/banner/banner";
+import Faq from "./components/faq/Faq";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import Services from "./components/services/Services";
+import Testimoniels from "./components/testimoniels/Testimoniels";
+import WhyChooseUs from "./components/WhyChooseUs/WhyChooseUs";
+import Footer from "./components/footer/Footer";
+
+import { IoIosArrowUp } from "react-icons/io";
+import { useEffect, useState } from "react";
 
 function App() {
   const menuLinks = [
@@ -15,18 +18,42 @@ function App() {
     "testimonials",
     "faq",
   ];
+  const [showBtnTop, setshowBtnTop] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (scrollY >= 300) {
+        setshowBtnTop(true);
+      } else {
+        setshowBtnTop(false);
+      }
+    });
+  }, []);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <>
-     <Header links={menuLinks}/>
-     <Hero/>
-     <WhyChooseUs/>
-     <Services/>
-     <Banner/>
-     <Testimoniels/>
-     <Faq/>
-     <Footer links={menuLinks}/>
+      {showBtnTop && (
+        <span
+          className="btn-back-top"
+          onClick={scrollToTop}
+        >
+          <IoIosArrowUp size={24} />
+        </span>
+      )}
+      <Header links={menuLinks} />
+      <Hero />
+      <WhyChooseUs />
+      <Services />
+      <Banner />
+      <Testimoniels />
+      <Faq />
+      <Footer links={menuLinks} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
